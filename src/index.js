dc59244c686e856e58d2813e1b1e76e53760f385
talkad <talkad@post.bgu.ac.il>
2022-06-12 15:30:15 +0300

    build

--- src/index.js
+++ src/index.js
@@ -14,6 +14,18 @@ import { CacheProvider } from "@emotion/react";
 import createCache from "@emotion/cache";
 import rtlPlugin from 'stylis-plugin-rtl';
 
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(3000);
+
 // for theming the website
 const theme = createTheme({
     palette: {
